9a23cf062907b32c999b69f03f189dba57a42551
moulineE <elmahdi.mouline@live.fr>
2024-01-05 18:34:04 +0100

    Task 6. Handle multiple promises -fix teste

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,7 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((result) => ({
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
     }));
