6c42b9fdf904d5c1f655281f6a0009615bbfc12e
moulineE <elmahdi.mouline@live.fr>
2024-01-05 18:55:55 +0100

    Task 6. Handle multiple promises -fix adding map to transform array of objects to a new array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,8 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => ({
+    .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : String(result.reason),
-    }));
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
