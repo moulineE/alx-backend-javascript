ead722befdba9d898160792b8158a90de5e39c29
moulineE <elmahdi.mouline@live.fr>
2024-01-15 16:48:25 +0100

    Task 11. Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  if (weakMap.get(endpoint) < 5) {
+    weakMap.set(endpoint, (weakMap.get(endpoint) + 1));
+  } else {
+    throw Error('Endpoint load is high');
+  }
+}
