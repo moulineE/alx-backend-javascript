a1c11063d5ba431450d7baabe8c3bffcaec43f49
moulineE <elmahdi.mouline@live.fr>
2024-01-15 16:55:27 +0100

    Task 11. Weak link data structure --fix error throwing wen query = 5

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,7 +4,7 @@ export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
-  if (weakMap.get(endpoint) < 5) {
+  if (weakMap.get(endpoint) < 4) {
     weakMap.set(endpoint, (weakMap.get(endpoint) + 1));
   } else {
     throw Error('Endpoint load is high');
