2fa747b0998502519e141b2272f52b9fac5a5ff7
moulineE <elmahdi.mouline@live.fr>
2024-02-27 22:28:58 +0100

    Task 4. Create a small HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+const server = http.createServer((req, res) => {
+  const responseText = 'Hello Holberton School!';
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.write(Buffer.from(responseText));
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
