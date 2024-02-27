77dbfb8cb2d2a479931ed45d3ad0527045ad455a
moulineE <elmahdi.mouline@live.fr>
2024-02-27 21:05:06 +0100

    Task 2. Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+
+const data = (path) => {
+  try {
+    return fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+const getField = (arr) => {
+  const fieldlist = [];
+  const counts = {};
+
+  for (let i = 1; i < arr.length; i += 1) {
+    fieldlist.push(arr[i].split(',')[3]);
+  }
+  for (const item of fieldlist) {
+    if (item in counts) {
+      counts[item] += 1;
+    } else {
+      counts[item] = 1;
+    }
+  }
+  return counts;
+};
+
+const getNameList = (arr, fields) => {
+  const dict = {};
+  for (const field of fields) {
+    dict[field] = [];
+    for (let i = 1; i < arr.length; i += 1) {
+      if (arr[i].split(',')[3] === field) {
+        dict[field].push(arr[i].split(',')[0]);
+      }
+    }
+  }
+  return dict;
+};
+
+const countStudents = (path) => {
+  const file = data(path);
+  const arr = file.split('\n');
+  const fieldsDict = getField(arr);
+  const fields = Object.keys(fieldsDict);
+  const nameByFieldDict = getNameList(arr, fields);
+  console.log(`Number of students: ${parseInt(arr.length, 10) - 1}`);
+  for (const field of fields) {
+    console.log(`Number of students in ${field}: ${fieldsDict[field]}. List: ${nameByFieldDict[field]}`);
+  }
+};
+
+module.exports = countStudents;
