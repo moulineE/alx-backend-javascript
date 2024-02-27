5f691d133892bdb4033f892e9058be6ce7e4965d
moulineE <elmahdi.mouline@live.fr>
2024-02-27 19:04:56 +0100

    Task 1. Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
