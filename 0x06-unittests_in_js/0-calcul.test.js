07544fa5996027d926d275e17c33977bd7644891
moulineE <elmahdi.mouline@live.fr>
2024-02-28 22:37:27 +0100

    Task 0. Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,13 @@
+const calculateNumber = require("./0-calcul");
+const assert = require('assert');
+
+describe('0-calcul.js calculateNumber Tests', function() {
+    describe('sum teste', function() {
+        it('should return the correct rounded sum', function() {
+            assert.equal(calculateNumber(1, 3), 4);
+            assert.equal(calculateNumber(1, 3.7), 5);
+            assert.equal(calculateNumber(1.2, 3.7), 5);
+            assert.equal(calculateNumber(1.5, 3.7), 6);
+        });
+    });
+});
