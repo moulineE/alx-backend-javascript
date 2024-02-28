bcfeeeed037ff8b4029780d94ade53473b48e037
moulineE <elmahdi.mouline@live.fr>
2024-02-28 02:25:45 +0100

    Task 8. Organize a complex HTTP server using Express -- in progress +

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,49 @@
+import readDatabase from '../utils';
+
+class StudentsController {
+  static getAllStudents(request, response) {
+    const path = process.argv.length > 2 ? process.argv[2] : '';
+    readDatabase(path)
+      .then((nameByFieldDict) => {
+        let output = 'This is the list of our students\n';
+        for (const field in nameByFieldDict) {
+          if (Object.prototype.hasOwnProperty.call(nameByFieldDict, field)) {
+            output += `Number of students in ${field}: ${nameByFieldDict[field].length()}. List: ${nameByFieldDict[field].join(', ')}\n`;
+          }
+        }
+        response.status(200).send(output.join('\n'));
+      })
+      .catch((err) => {
+        response
+          .status(500)
+          .send(err instanceof Error ? err.message : err.toString());
+      });
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    const path = process.argv.length > 2 ? process.argv[2] : '';
+    const { major } = request.params;
+    const validator = ['CS', 'SWE'];
+
+    if (!validator.includes(major)) {
+      response
+        .status(500)
+        .send('Major parameter must be CS or SWE');
+    }
+    readDatabase(path)
+      .then((nameByFieldDict) => {
+        let output = '';
+        if (Object.keys(nameByFieldDict).includes(major)) {
+          output += `List: ${nameByFieldDict[major].join(', ')}\n`;
+        }
+        response.status(200).send(output.join('\n'));
+      })
+      .catch((err) => {
+        response
+          .status(500)
+          .send(err instanceof Error ? err.message : err.toString());
+      });
+  }
+}
+
+module.exports = StudentsController;
